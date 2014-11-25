568484a78949d5db598ef161c4048bce23da8729
Firstname Lastname <645382493@qq.com>
2014-11-25 17:33:21 +0800

    mobile
    aotumation

---
+++ Config.java
@@ -0,0 +1,29 @@
+package com.ctrip.view.LearnTest;
+
+import java.io.File;
+import java.net.URL;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class Config {
+	public static  AndroidDriver driver; 
+	public void setUp() throws Exception {  
+        //配置appium参数  
+        File classpathRoot = new File(System.getProperty("user.dir"));  
+        File appDir = new File(classpathRoot, "apps");  
+        File app = new File(appDir, "Ctrip_V5.10_SIT7_PRODUCT.apk");  
+        DesiredCapabilities capabilities = new DesiredCapabilities();  
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");  
+        capabilities.setCapability("platformName", "Android");  
+        capabilities.setCapability("deviceName","4df72e3d1fbb30af");  
+        capabilities.setCapability("platformVersion", "4.3");  
+        capabilities.setCapability("app", app.getAbsolutePath());  
+        capabilities.setCapability("appPackage", "ctrip.android.view");
+        capabilities.setCapability("appActivity", ".home.CtripSplashActivity");
+        //capabilities.setCapability("automationName", "selendroid"); 
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);  
+    }
+}
---
+++ UIutil.java
@@ -0,0 +1,67 @@
+package com.ctrip.view.LearnTest;
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class UIutil {
+	
+	//判断元素是否存在	
+    public boolean IsExistElement(String str)
+    {
+        try{
+        	
+            Thread.sleep(3000);
+            WebElement el = Config.driver.findElement(By.name(str));  
+            if (el.isEnabled())
+            {
+                return true;
+            }
+            return false;
+        }catch(Exception e){
+            return false;
+        }
+    }
+    //判断是否存在更新
+    public void IsExistUpdate()
+    {
+    if (IsExistElement("以后再说"))
+    {
+    	Config.driver.findElement(By.name("以后再说")).click();
+    }
+    }
+    //判断是否登录
+    public boolean IsLogin()
+    {
+    	try{
+        	
+            Thread.sleep(3000);
+            WebElement el = Config.driver.findElement(By.name("登录/注册"));  
+            if (el.isEnabled())
+            {
+                return false;
+            }
+            return true;
+        }catch(Exception e){
+            return true;
+        }
+    }
+    //用户登录
+    public void UserLogin()
+    {
+    	WebElement el = Config.driver.findElement(By.name("登录/注册"));
+    	el.click();
+        List<WebElement> textFieldsList = Config.driver.findElementsByClassName("android.widget.EditText");  
+        /*
+        appium不支持中文输入 参考了robotium的以js方式为元素直接设置value的做法
+                利用Selenium中Webdriver执行js方法实现中文输入
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("document.getElementById('word').value='输入法'");
+        */
+        textFieldsList.get(0).sendKeys("wwwwww");  
+        textFieldsList.get(1).sendKeys("good08");  
+        Config.driver.findElementByName("登录").click(); 
+    }
+}
