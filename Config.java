718b72b6707b1659c2cf35ae570a725ee603bdfb
dctang <dctang@Ctrip.com>
2014-12-30 16:31:40 +0800

    commit by tangdongchu
    更新config参数化，从XML读取相关配置

--- Config.java
+++ Config.java
@@ -1,7 +1,12 @@
-﻿package com.ctrip.view.LearnTest;
+package com.ctrip.view.LearnTest;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Iterator;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -9,20 +14,47 @@ import io.appium.java_client.android.AndroidDriver;
 
 public class Config {
 	public static  AndroidDriver driver; 
-	public void setUp() throws Exception {  
-        //配置appium参数  
-        File classpathRoot = new File(System.getProperty("user.dir"));  
-        File appDir = new File(classpathRoot, "apps");  
-        File app = new File(appDir, "Ctrip_V5.10_SIT7_PRODUCT.apk");  
+	public void setUp() throws Exception {
+		
+        //配置从XML读取appium参数
+		Config config = new Config();
+        String BrowserName = config.readxml("BrowserName");
+        String platformName = config.readxml("PlatformName");
+        String deviceName = config.readxml("DeviceName");
+        String platformVersion = config.readxml("PlatformVersion");
+        String app = config.readxml("ApkPath");
+        String appPackage = config.readxml("appPackage");
+        String appActivity = config.readxml("appActivity");
+        //String automationName = config.readxml("automationName");
         DesiredCapabilities capabilities = new DesiredCapabilities();  
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");  
-        capabilities.setCapability("platformName", "Android");  
-        capabilities.setCapability("deviceName","4df72e3d1fbb30af");  
-        capabilities.setCapability("platformVersion", "4.3");  
-        capabilities.setCapability("app", app.getAbsolutePath());  
-        capabilities.setCapability("appPackage", "ctrip.android.view");
-        capabilities.setCapability("appActivity", ".home.CtripSplashActivity");
-        //capabilities.setCapability("automationName", "selendroid"); 
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserName);  
+        capabilities.setCapability("platformName", platformName);  
+        capabilities.setCapability("deviceName", deviceName);  
+        capabilities.setCapability("platformVersion", platformVersion);  
+        capabilities.setCapability("app", app);  
+        capabilities.setCapability("appPackage", appPackage);
+        capabilities.setCapability("appActivity", appActivity);
+        //capabilities.setCapability("automationName", automationName); 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);  
     }
+	
+	public String readxml(String key) throws Exception {  
+		SAXReader reader = new SAXReader();
+		File classpathRoot = new File(System.getProperty("user.dir"));   
+		File file = new File(classpathRoot, "Config.xml");//创建file对象
+		Document document = reader.read(file);// 读取XML文件
+		Element root = document.getRootElement();// 得到根节点
+		String value = null;
+		for (Iterator i = root.elementIterator("add"); i.hasNext();) {
+			Element account = (Element) i.next();//遍历整个XML
+			if (account.attributeValue("key").equals(key)) {
+				// 读取value
+				value = account.attributeValue("value");
+				break;
+				}
+			}
+		return value;
+		}
 }
+
+
